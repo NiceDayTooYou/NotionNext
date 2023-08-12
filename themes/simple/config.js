3e499b68870dd1d048101012ba3166941fd692bc
NiceDayToYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-12 13:54:11 +0800

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   LOGO_IMG: '/Logo.webp',
   TOP_BAR: true, // 显示顶栏
   TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>编程爱好者<br/>/互联网从业者<br/>/知识分享博主</div>',
+  LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>昨天是段历史<br/>/明天是个谜团<br/>/而今天是天赐的礼物</div>',
 
   AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
