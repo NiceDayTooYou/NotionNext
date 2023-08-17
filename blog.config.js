7f48d4e892d800faca8dc58e4faf083cf0d6096d
NiceDayToYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-17 13:26:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -310,13 +310,6 @@ const BLOG = {
     TOKEN: process.env.NEXT_PUBLIC_WEBMENTION_TOKEN || ''
   },
 
-    // 标签卖萌
-const diytitle = {
-  enable: true,
-  leaveTitle: "w(ﾟДﾟ)w 不要走！再看看嘛！",
-  backTitle: "♪(^∇^*)欢迎肥来!"
-};
-
   // <---- 评论插件
 
   // ----> 站点统计
