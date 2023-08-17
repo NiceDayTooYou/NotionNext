38d0eccd1eb92ff7dd4f188b489f526176abc13d
NiceDayToYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-17 18:39:39 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,14 @@
-var OriginTitile=document.title,jiao;
+var OriginTitile = document.title;
+var jiao;
+
 document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState == 'hidden') {
+    if (document.visibilityState === undefined || document.visibilityState === 'visible') {
+        document.title = '♪(^∇^*)欢迎肥来' + OriginTitile;
+        jiao = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 4000);
+    } else {
         document.title = '不要走！再看看嘛';
         clearTimeout(jiao);
-    } 
-    else {
-        document.title = '欢迎肥来！'+ OriginTitile;
-        jiao=setTimeout(function(){
-            document.title=OriginTitile;
-        },4000);
     }
 });
