e548b6c1e721c5e007539fdee25aa6601599d632
NiceDayToYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-17 18:40:26 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,7 +3,7 @@ var jiao;
 
 document.addEventListener('visibilitychange', function () {
     if (document.visibilityState === undefined || document.visibilityState === 'visible') {
-        document.title = '♪(^∇^*)欢迎肥来' + OriginTitile;
+        document.title = '欢迎肥来！' + OriginTitile;
         jiao = setTimeout(function () {
             document.title = OriginTitile;
         }, 4000);
