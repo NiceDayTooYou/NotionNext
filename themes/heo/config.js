37bd50f0fcc2ec182bf343a0d5cf2fc8c7ede574
NiceDayToYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-20 00:33:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -101,7 +101,7 @@ const CONFIG = {
   ],
 
   SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  SOCIAL_CARD_TITLE_1: 'About me☃️',
+  SOCIAL_CARD_TITLE_1: 'About me',
   SOCIAL_CARD_TITLE_2: 'Thanks for reading',
   SOCIAL_CARD_TITLE_3: 'Click to view the profile and find out more',
   SOCIAL_CARD_URL: 'https://flowcv.com/resume/ww2mhitt2c',
