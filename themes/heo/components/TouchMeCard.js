8b370a221c5d9414f6785b8e13a3539ec5520fd9
NiceDayToYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-20 00:03:46 +0800

    Update TouchMeCard.js

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -20,7 +20,7 @@ export default function TouchMeCard() {
                     <div className='h-full'>
                         <h2 className='font-[1000] text-3xl'>{CONFIG.SOCIAL_CARD_TITLE_1}</h2>
                         <h3 className='pt-2'>{CONFIG.SOCIAL_CARD_TITLE_2}</h3>
-                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://s1.ax1x.com/2023/08/19/pP8Mr26.png) center center no-repeat' }}></div>
+                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://s1.ax1x.com/2023/08/20/pP8MxGq.png) center center no-repeat' }}></div>
                     </div>}
                 backContent={<Link href={CONFIG.SOCIAL_CARD_URL}>
                     <div className='font-[1000] text-xl h-full'>
