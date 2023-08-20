6dae867704c8f3ad01da2510d5d39b6713c0095e
NiceDayToYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-20 16:39:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,13 +13,13 @@
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，昨天是段历史😶, Hi，明天是个谜团😶‍🌫️,Hi，今天是天赐的礼物❤️',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Do you know?，昨天是段历史😶, Do you know?，明天是个谜团😶‍🌫️,Do you know?，今天是天赐的礼物❤️，So要像珍惜礼物那样珍惜今天',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Li Yong', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个平凡的普通人👻', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://space.bilibili.com/1375298909/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://github.com/NiceDayTooYou', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Li Yong', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
