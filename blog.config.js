9ee50100e92d536f13de49146ddc200abfb2f1c1
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-21 22:41:02 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,10 +17,10 @@
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Li Yong', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个平凡的普通人👻', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://github.com/NiceDayTooYou', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '和美好不期而遇', // 网站关键词 英文逗号隔开
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Serendipity', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '和美好不期而遇👻', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://flowcv.com/resume/ww2mhitt2c', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'LiYong', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'nicedaytoooyou@gmail.com', // 邮箱地址 例如mail@tangly1024.com
