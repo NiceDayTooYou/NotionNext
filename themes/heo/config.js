e950cc8b1d1b5929f15699c7af6f675c7d6c07c6
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-06 20:08:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -53,7 +53,7 @@ const CONFIG = {
     {
       title_1: 'Docker',
       img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#1f1f1f',
+      color_1: '#ffffff',
       title_2: 'Photoshop',
       img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
       color_2: '#ffffff'
@@ -69,7 +69,7 @@ const CONFIG = {
     {
       title_1: 'Swift',
       img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#1f1f1f',
+      color_1: '#ffffff',
       title_2: 'twitter',
       img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
       color_2: '#ffffff'
@@ -80,23 +80,23 @@ const CONFIG = {
       color_1: '#ffffff',
       title_2: 'CSS3',
       img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
-      color_2: '#1f1f1f'
+      color_2: '#ffffff'
     },
     {
       title_1: 'JS',
       img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#1f1f1f',
+      color_1: '#ffffff',
       title_2: 'HTML',
       img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
-      color_2: '#1f1f1f'
+      color_2: '#ffffff'
     },
     {
       title_1: 'Git',
       img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#1f1f1f',
+      color_1: '#ffffff',
       title_2: 'Rhino',
       img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
-      color_2: '#1f1f1f'
+      color_2: '#ffffff'
     }
   ],
 
