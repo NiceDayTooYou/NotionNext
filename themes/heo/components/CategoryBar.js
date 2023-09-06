2f1e3522d8cdba9c5b4016263e98a8c4e7fd495b
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-06 22:58:40 +0800

    Update CategoryBar.js

--- themes/heo/components/CategoryBar.js
+++ themes/heo/components/CategoryBar.js
@@ -29,7 +29,7 @@ export default function CategoryBar(props) {
     }
   }
   return <div id='category-bar' className={`flex flex-nowrap justify-between items-center h-12 mb-4 space-x-2 w-full lg:bg-white dark:lg:bg-[#1e1e1e]  
-  ${border ? 'lg:border lg:hover:border dark:lg:border-gray-800 hover:border-indigo-600 dark:hover:border-yellow-600 ' : ''}  py-2 lg:px-2 rounded-xl transition-colors duration-200`}>
+  ${border ? 'lg:border lg:hover:border dark:lg:border-gray-900 hover:border-gray-900 dark:hover:border-green-400 ' : ''}  py-2 lg:px-2 rounded-xl transition-colors duration-200`}>
 
         <div id='category-bar-items' ref={categoryBarItemsRef} className='scroll-smooth max-w-4xl rounded-lg scroll-hidden flex justify-start flex-nowrap items-center overflow-x-scroll'>
             <MenuItem href='/' name={locale.NAV.INDEX} />
@@ -40,7 +40,7 @@ export default function CategoryBar(props) {
             <div id='right' className='cursor-pointer mx-2' onClick={handleToggleScroll}>
             {scrollRight ? <ChevronDoubleLeft className={'w-5 h-5'} /> : <ChevronDoubleRight className={'w-5 h-5'} /> }
             </div>
-            <Link href='/category' className='whitespace-nowrap font-bold text-gray-900 dark:text-white transition-colors duration-200 hover:text-indigo-600'>
+            <Link href='/category' className='whitespace-nowrap font-bold text-white dark:text-white transition-colors duration-200 hover:text-white'>
                 {locale.MENU.CATEGORY}
             </Link>
         </div>
