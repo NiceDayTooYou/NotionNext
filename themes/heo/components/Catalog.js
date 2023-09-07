344fda27623475942a4658d2f922d4d46ae811e7
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-07 08:23:37 +0800

    Update Catalog.js

--- themes/heo/components/Catalog.js
+++ themes/heo/components/Catalog.js
@@ -70,11 +70,11 @@ const Catalog = ({ toc }) => {
             <a
               key={id}
               href={`#${id}`}
-              className={`notion-table-of-contents-item duration-300 transform font-light dark:text-gray-200
+              className={`notion-table-of-contents-item duration-300 transform font-light dark:text-gray-50
             notion-table-of-contents-item-indent-level-${tocItem.indentLevel} `}
             >
               <span style={{ display: 'inline-block', marginLeft: tocItem.indentLevel * 16 }}
-                className={`${activeSection === id && ' font-bold text-indigo-600'}`}
+                className={`${activeSection === id && ' font-bold text-green-600'}`}
               >
                 {tocItem.text}
               </span>
