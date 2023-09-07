8685f6a5c26e03720def2476316a6ec024a73334
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-07 12:28:55 +0800

    Update NavBar.js

--- themes/heo/components/NavBar.js
+++ themes/heo/components/NavBar.js
@@ -129,7 +129,7 @@ const NavBar = props => {
         `}</style>
 
         {/* 顶部导航菜单栏 */}
-        <nav id='nav' className={`${fixedNav ? 'fixed' : 'relative bg-none'} ${textWhite ? 'text-white ' : 'text-black dark:text-white'}  ${navBgWhite ? 'bg-white dark:bg-[#1a191d]' : 'bg-none'} z-20 h-16 top-0 w-full`}>
+        <nav id='nav' className={`${fixedNav ? 'fixed' : 'relative bg-none'} ${textWhite ? 'text-white ' : 'text-black dark:text-white'}  ${navBgWhite ? 'bg-white dark:bg-[#000000]' : 'bg-none'} z-20 h-16 top-0 w-full`}>
             <div className='flex h-full mx-auto justify-between items-center max-w-[86rem] px-8'>
                 {/* 左侧logo */}
                 <div className='flex'>
