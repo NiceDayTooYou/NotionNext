b8c53bd65bd1558fce01b85f8dbb472456dfb574
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-07 12:40:34 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -20,7 +20,7 @@ export function InfoCard(props) {
   const isSlugPage = router.pathname === '/[...slug]'
 
   return (
-        <Card className='bg-[#ffffff] dark:bg-gray-900 text-white flex flex-col w-72 overflow-hidden relative'>
+        <Card className='bg-[#eeecfc] dark:bg-gray-900 text-white flex flex-col w-72 overflow-hidden relative'>
             {/* 信息卡牌第一行 */}
             <div className='flex justify-between'>
                 {/* 问候语 */}
