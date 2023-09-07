047e744a1ded669ac23a59bc2bfb4381f752e317
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-07 11:03:25 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -25,7 +25,7 @@ export function InfoCard(props) {
             <div className='flex justify-between'>
                 {/* 问候语 */}
                 <GreetingsWords />
-                <div className={`${isSlugPage ? 'absolute right-0 -mt-8 -mr-5 hover:opacity-0 hover:scale-150 blur' : 'cursor-pointer'} justify-center items-center flex dark:text-gray-100 transform transitaion-all duration-200`}>
+                <div className={`${isSlugPage ? 'absolute right-0 -mt-8 -mr-5 hover:opacity-0 hover:scale-150 blur' : 'cursor-pointer'} justify-center items-center flex dark:text-gray-50 transform transitaion-all duration-200`}>
                     <LazyImage src={siteInfo?.icon} className='rounded-full' width={isSlugPage ? 100 : 28} alt={BLOG.AUTHOR} />
                 </div>
             </div>
