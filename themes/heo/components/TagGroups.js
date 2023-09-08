4fa992d771c3cbe1582f04d71f0718d34cfb3854
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-08 20:26:20 +0800

    Update TagGroups.js

--- themes/heo/components/TagGroups.js
+++ themes/heo/components/TagGroups.js
@@ -10,7 +10,7 @@ import Link from 'next/link'
 const TagGroups = ({ tags, className }) => {
   if (!tags) return <></>
   return (
-        <div id='tags-group' className='dark:border-gray-700 space-y-2'>
+        <div id='tags-group' className='dark:border-gray-50 space-y-2'>
             {
                 tags.map((tag, index) => {
                   return <Link passHref
