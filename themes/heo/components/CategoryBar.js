200a225424c87b8b24297a99cd95314766ebc5d6
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-08 21:55:30 +0800

    Update CategoryBar.js

--- themes/heo/components/CategoryBar.js
+++ themes/heo/components/CategoryBar.js
@@ -55,7 +55,7 @@ export default function CategoryBar(props) {
 const MenuItem = ({ href, name }) => {
   const router = useRouter()
   const selected = router.pathname === href
-  return <div className={`whitespace-nowrap mr-2 duration-200 transition-all font-bold px-2 py-0.5 rounded-md text-gray-900 dark:text-white hover:text-white hover:bg-geay-900 dark:hover:bg-gray-50 ${selected ? 'text-white bg-gray-900 dark:bg-gray-50' : ''}`}>
+  return <div className={`whitespace-nowrap mr-2 duration-200 transition-all font-bold px-2 py-0.5 rounded-md text-gray-900 dark:text-white hover:text-white hover:bg-red-600 dark:hover:bg-green-500 ${selected ? 'text-white bg-red-600 dark:bg-green-500' : ''}`}>
         <Link href={href}>{name}</Link>
     </div>
 }
