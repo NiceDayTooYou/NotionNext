f0313a54e43e358b6409c7a87ea46a8bcd118aa6
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-15 18:11:13 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -12,3 +12,45 @@ document.addEventListener('visibilitychange', function () {
         clearTimeout(jiao);
     }
 });
+
+/**
+ * 封装文字弹出的函数
+ * @param {*} arr
+ * @param {*} options
+ * @returns
+ */
+const fnTextPopup = function (arr, options) {
+  // arr参数是必须的
+  if (!arr || !arr.length) {
+    return
+  }
+  // 主逻辑
+  let index = 0
+  document.documentElement.addEventListener('click', function (event) {
+    const x = event.pageX; const y = event.pageY
+    const eleText = document.createElement('span')
+    // 随机颜色
+    eleText.style.color = 'rgb(' + 255 * Math.random() + ',' + 255 * Math.random() + ',' + 255 * Math.random() + ')'
+    // 动画样式
+    eleText.className = 'text-popup'
+    this.appendChild(eleText)
+    if (arr[index]) {
+      eleText.innerHTML = arr[index]
+    } else {
+      index = 0
+      eleText.innerHTML = arr[0]
+    }
+    // 动画结束后删除自己
+    eleText.addEventListener('animationend', function () {
+      eleText.parentNode.removeChild(eleText)
+    })
+    // 位置
+    eleText.style.left = (x - eleText.clientWidth / 2) + 'px'
+    eleText.style.top = (y - eleText.clientHeight) + 'px'
+    // index递增
+    index++
+  })
+}
+
+// 执行，传入文字内容
+fnTextPopup(['❤', '🙂', '💩', '👻', '👀', '🪴', '🍉', '☃️', '🎄', '🎈', '🎯', '💯'])
